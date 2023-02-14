26e10e5a58c0d55df558518127c3875450341719
Kahve <72060765+KahweB@users.noreply.github.com>
2023-02-14 05:52:34 +0300

    add: screen populate, eventlisteners

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+let screen = document.getElementById("screen");
+let button = document.querySelectorAll("button");
+function onClick(elem){
+    
+    switch (elem.target.value) {
+        case "1":
+            screen.innerHTML += "1";
+            break;
+        case "2":
+            screen.innerHTML += "2";
+            break;
+        case "3":
+            screen.innerHTML += "3";
+            break;
+        case "4":
+            screen.innerHTML += "4";
+            break;
+        case "5":
+            screen.innerHTML += "5";
+            break;
+        case "6":
+            screen.innerHTML += "6";
+            break;
+        case "7":
+            screen.innerHTML += "7";
+            break;
+        case "8":
+            screen.innerHTML += "8";
+            break;
+        case "9":
+            screen.innerHTML += "9";
+            break;
+        case "0":
+            screen.innerHTML += "0";
+            break;
+        default:
+            break;
+        
+    }
+}
+button.forEach((element) => {
+    element.addEventListener("click", onClick);
+})
+
+
+
+
 function add(numberOne,numberTwo) {
     return numberOne+numberTwo;
 }
